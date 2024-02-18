cbabd2d8776656e1e1706a6b46526e70625353e5
Akif <kosusakifemre@gmail.com>
2024-02-18 20:57:03 +0300

    Firebase problem in chat fixed

--- src/screens/Chat.js
+++ src/screens/Chat.js
@@ -1,63 +1,91 @@
 import { auth } from '../firebase/firebase'
-import React, { useCallback, useLayoutEffect, useState,useEffect } from 'react'
+import React, { useCallback, useLayoutEffect, useState, useEffect } from 'react'
 import { GiftedChat } from 'react-native-gifted-chat'
-import { db } from '../firebase/firebase';
-const Chat = () => {
-const [messages, setMessages] = useState([]);
+import { firestore } from '../firebase/firebase';
+import { doc, getDoc, collection, getDocs, query, orderBy, limit, setDoc } from 'firebase/firestore';
+const Chat = ({ navigation }) => {
+  const [messages, setMessages] = useState([{
+    _id: 1,
+    text: 'Hello',
+    createdAt: new Date(),
+    user: {
+      _id: 2,
+      name: 'React Native',
+      avatar: 'https://placeimg.com/140/140/any',
+    },
+  }]);
+
+  const getChats = async () => {
+    const chatsRef = collection(firestore, "chats");
+    const q = query(chatsRef, orderBy('createdAt', 'desc'));
+    const querySnapshot = await getDocs(q);
+    const newMessages = [...messages];
+    querySnapshot.forEach((doc) => {
+      const data = doc.data();
+      const _id = data._id;
+      const createdAt = data.createdAt.toDate();
+      const text = data.text;
+      const user = data.user;
+
+      // doc.data() is never undefined for query doc snapshots
+      newMessages.push({
+        _id: _id,
+        createdAt: createdAt,
+        text: text,
+        user: user,
+      });
+      setMessages(newMessages);
+    });
+  }
 
   useEffect(() => {
-    setMessages([
-      {
-        _id: 1,
-        text: 'Hello',
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'React Native',
-          avatar: 'https://placeimg.com/140/140/any',
-        },
-      },
-    ])
-  }, [])
-  useLayoutEffect(() => {
-    const unsubscribe = db.collection('chats').orderBy('createdAt', 'desc').onSnapshot(snapshot => setMessages(
-    snapshot.docs.map(doc => ({
-    _id: doc.data()._id,
-    createdAt: doc.data().createdAt.toDate(),
-    text: doc.data().text,
-    user: doc.data().user,
-    }))
-    ));
+    const unsubscribe = navigation.addListener("focus", () => {
+      getChats();
+    });
+
     return unsubscribe;
-    }, [])
-  const onSend = useCallback((messages = []) => {
+  }, [navigation]);
+  /* useLayoutEffect(() => {
+
+    
+     const unsubscribe = firestore.collection('chats').orderBy('createdAt', 'desc').onSnapshot(snapshot => setMessages(
+      snapshot.docs.map(doc => ({
+        _id: doc.data()._id,
+        createdAt: doc.data().createdAt.toDate(),
+        text: doc.data().text,
+        user: doc.data().user,
+      }))
+    )); 
+    return unsubscribe;
+  }, []) */
+  const onSend = useCallback(async (messages = []) => {
     setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
-    const{
+    const {
       _id,
       createdAt,
       text,
       user
 
     } = messages[0]
-    db.collection('chats').add({
-      _id,
-      createdAt,
-      text,
-      user
-      })
+    await setDoc(doc(firestore, "chats", _id), {
+      _id: _id,
+      createdAt: createdAt,
+      text: text,
+      user: user,
+    });
   }, [])
 
   return (
-<GiftedChat
-messages={messages}
-showAvatarForEveryMessage={true}
-onSend={messages => onSend(messages)}
-user={{
-_id: auth?.currentUser?.email,
-name: auth?.currentUser?.displayName,
-avatar: auth?.currentUser?.photoURL
-}}
-/>
+    <GiftedChat
+      messages={messages}
+      showAvatarForEveryMessage={true}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: auth?.currentUser?.email,
+        name: auth?.currentUser?.displayName,
+        avatar: auth?.currentUser?.photoURL
+      }}
+    />
   )
 }
-  export default Chat
+export default Chat
